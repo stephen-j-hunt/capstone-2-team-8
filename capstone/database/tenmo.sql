34c39ae848feb6579f7cbf08b75f69bfee0549b2
elliot tumbleson <elliot41403@gmail.com>
2023-03-08 11:09:23 -0500

    DatabaseDesignChangesV1

--- capstone/database/tenmo.sql
+++ capstone/database/tenmo.sql
@@ -1,5 +1,7 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS tenmo_transaction;
+DROP TABLE IF EXISTS account;
 DROP TABLE IF EXISTS tenmo_user;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -16,5 +18,21 @@ CREATE TABLE tenmo_user (
 	CONSTRAINT PK_tenmo_user PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username)
 );
-
+CREATE TABLE account (
+	account_id SERIAL NOT NULL PRIMARY KEY,
+	user_id int NOT NULL,
+	balance real NOT NULL DEFAULT 1000.00,
+	CONSTRAINT FK_account_user_id FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+CREATE TABLE tenmo_transaction (
+	transfer_id SERIAL NOT NULL PRIMARY KEY,
+	sender_id int NOT NULL,
+	receiver_id int NOT NULL,
+	is_request boolean NOT NULL,
+	status boolean NOT NULL,
+	amount real NOT NULL,
+	time datetime NOT NULL
+	CONSTRAINT FK_tenmo_transaction_sender_id FOREIGN KEY (sender_id) REFERENCES account(account_id),
+	CONSTRAINT FK_tenmo_transaction_receiver_id FOREIGN KEY (receiver_id) REFERENCES account(account_id)
+);
 COMMIT;
