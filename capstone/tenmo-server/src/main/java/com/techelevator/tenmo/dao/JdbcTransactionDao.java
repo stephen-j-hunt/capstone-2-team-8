e62e274942fe410741e2b006ca948d71ad3ac394
elliot tumbleson <elliot41403@gmail.com>
2023-03-09 09:27:30 -0500

    Implemented constraints for sending money

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransactionDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransactionDao.java
@@ -1,5 +1,6 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transaction;
 
 import java.math.BigDecimal;
@@ -9,10 +10,23 @@ public class JdbcTransactionDao implements TransactionDao{
     @Override
     public BigDecimal sendMoney(int sendId, int receiveId, BigDecimal amount) {
       /*
-      Cant send to self
+      Cant send to self *
       Cannot be negative or zero
-      Amount can not surpas balance
+      Amount can not surpass balance*
        */
+        Account account = null;
+        if(sendId != receiveId){
+            BigDecimal zero = BigDecimal.ZERO;
+            int res = amount.compareTo(zero);
+            if(res == 1){
+                int result = amount.compareTo(account.getBalance());
+                if(result == 0 || result == -1){
+
+                }
+
+            }
+
+        }
         return null;
     }
 
